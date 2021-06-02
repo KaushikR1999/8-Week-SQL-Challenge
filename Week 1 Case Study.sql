23c3345538085d73c4108659f902b0df7629afff
KaushikR1999 <58514719+KaushikR1999@users.noreply.github.com>
2021-06-02 23:13:09 +0800

    Update Week 1 Case Study.sql

--- Week 1 Case Study.sql
+++ Week 1 Case Study.sql
@@ -11,3 +11,18 @@ GROUP BY SALES.CUSTOMER_ID
 SELECT CUSTOMER_ID, COUNT(DISTINCT ORDER_DATE)
 FROM SALES
 GROUP BY CUSTOMER_ID
+
+-- 3. What was the first item from the menu purchased by each customer?
+
+SELECT SALES.CUSTOMER_ID, MENU.PRODUCT_NAME, RANK() OVER(PARTITION BY SALES.CUSTOMER_ID ORDER BY SALES.ORDER_DATE DESC) AS RowNumber 
+FROM SALES
+JOIN MENU
+ON SALES.PRODUCT_ID = MENU.PRODUCT_ID
+
+-- 4. What is the most purchased item on the menu and how many times was it purchased by all customers?
+-- 5. Which item was the most popular for each customer?
+-- 6. Which item was purchased first by the customer after they became a member?
+-- 7. Which item was purchased just before the customer became a member?
+-- 8. What is the total items and amount spent for each member before they became a member?
+-- 9.  If each $1 spent equates to 10 points and sushi has a 2x points multiplier - how many points would each customer have?
+-- 10. In the first week after a customer joins the program (including their join date) they earn 2x points on all items, not just sushi - how many points do customer A and B have at the end of January?
