c3502a5183afba81d0977641c14524037765c9c5
KaushikR1999 <58514719+KaushikR1999@users.noreply.github.com>
2021-06-04 02:45:06 +0800

    Update Week 1 Case Study.sql

--- Week 1 Case Study.sql
+++ Week 1 Case Study.sql
@@ -1,25 +1,36 @@
 -- 1. What is the total amount each customer spent at the restaurant?
 
-SELECT SALES.CUSTOMER_ID, SUM(PRICE)
-FROM SALES
-INNER JOIN MENU
-ON SALES.PRODUCT_ID = MENU.PRODUCT_ID
-GROUP BY SALES.CUSTOMER_ID
+SELECT sales.customer_id, SUM(menu.price)
+FROM sales
+INNER JOIN menu
+ON sales.product_id = menu.product_id
+GROUP BY sales.customer_id
 
 -- 2. How many days has each customer visited the restaurant?
 
-SELECT CUSTOMER_ID, COUNT(DISTINCT ORDER_DATE)
-FROM SALES
-GROUP BY CUSTOMER_ID
+SELECT customer_id, COUNT(DISTINCT order_date)
+FROM sales
+GROUP BY customer_id
 
 -- 3. What was the first item from the menu purchased by each customer?
 
-SELECT SALES.CUSTOMER_ID, MENU.PRODUCT_NAME, RANK() OVER(PARTITION BY SALES.CUSTOMER_ID ORDER BY SALES.ORDER_DATE DESC) AS RowNumber 
-FROM SALES
-JOIN MENU
-ON SALES.PRODUCT_ID = MENU.PRODUCT_ID
+SELECT customer_id, product_name
+FROM (SELECT sales.customer_id, menu.product_name, DENSE_RANK () OVER (PARTITION BY sales.customer_id ORDER BY sales.order_date) AS rownumber
+FROM sales
+JOIN menu
+ON sales.product_id = menu.product_id) AS tab
+WHERE rownumber = 1
 
 -- 4. What is the most purchased item on the menu and how many times was it purchased by all customers?
+
+SELECT menu.product_name, COUNT(*)
+FROM sales
+JOIN menu
+ON sales.product_id = menu.product_id
+GROUP BY menu.product_name
+ORDER BY COUNT(*) DESC
+LIMIT 1
+
 -- 5. Which item was the most popular for each customer?
 -- 6. Which item was purchased first by the customer after they became a member?
 -- 7. Which item was purchased just before the customer became a member?
